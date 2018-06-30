3b2f62f02ed71376ff7e5ea66cde3313570a2efa
ThiagoVSouza <35876587+ThiagoVSouza@users.noreply.github.com>
2018-06-30 17:53:47 -0300

    Update auction.sol

--- auction/auction.sol
+++ auction/auction.sol
@@ -21,7 +21,7 @@ contract auction
 	uint end_auction;
 	
 	
-	constructor(address _mediator, uint _mediator_fee, uint _max_bid) public payable{
+    	constructor(address _mediator, uint _mediator_fee, uint _max_bid) public payable{
     
 	    seller = msg.sender;
 	    
@@ -31,52 +31,52 @@ contract auction
 	    mediator_fee = _mediator_fee;
 	    max_bid = _max_bid;
 	
-	}
+    	}
 	
-	function check_balance() public returns(uint){
+    	function check_balance() public returns(uint){
         
-        return this.balance;
+        	return this.balance;
         
-    }
+    	}
     
-    function deposit() payable public{
+    	function deposit() payable public{
         
-        require(status != 3);
+        	require(status != 3);
         
-    }
+    	}
 	
-	function change_seller(address _future_seller) public{
+    	function change_seller(address _future_seller) public{
         
-        require(seller == msg.sender);
-        require(status == 1);
-        
-        seller = _future_seller;
-        
-    }
+		require(seller == msg.sender);
+		require(status == 1);
+
+		seller = _future_seller;
+
+	}
 	
 	function change_mediator(address _future_mediator, uint _future_fee) public{
         
-        require(seller == msg.sender);
-        require(status == 1 || status == 2);
+		require(seller == msg.sender);
+		require(status == 1 || status == 2);
+
+		future_mediator = _future_mediator;
+		future_fee = _future_fee;
+		mediator_aproved = 1;
         
-        future_mediator = _future_mediator;
-        future_fee = _future_fee;
-        mediator_aproved = 1;
-        
-    }
+    	}
 	
-	function aprove_mediator() public{
+    	function aprove_mediator() public{
         
-        require(bidder == msg.sender);
-        require(mediator_aproved == 1);
-        require(status != 3);
-        
-        mediator = future_mediator;
-        mediator_fee = future_fee;
-        
-        mediator_aproved = 0;
-        
-    }
+		require(bidder == msg.sender);
+		require(mediator_aproved == 1);
+		require(status != 3);
+
+		mediator = future_mediator;
+		mediator_fee = future_fee;
+
+		mediator_aproved = 0;
+
+    	}
 	
 	/// Bid
 	
@@ -103,98 +103,96 @@ contract auction
 	function accept_bid() public{
     
 		require(seller == msg.sender);
-        require(status == 1);
-        require( this.balance >= ( current_bid + mediator_fee ) );
-        
-	    end_auction = 1;
+		require(status == 1);
+		require( this.balance >= ( current_bid + mediator_fee ) );
+
+	    	end_auction = 1;
 	    
 	}
 	
 	function confirm_bid() public{
     
-        require(bidder == msg.sender);
-        require(status == 1);
-        require(end_auction == 1);
-        require( this.balance >= ( current_bid + mediator_fee ) );
+		require(bidder == msg.sender);
+		require(status == 1);
+		require(end_auction == 1);
+		require( this.balance >= ( current_bid + mediator_fee ) );
+
+		seller.transfer(current_bid);
         
-        seller.transfer(current_bid);
+		if(mediation_invoked == 1){
+
+		    mediator.transfer(mediator_fee);
+
+		}
         
-        if(mediation_invoked == 1){
-            
-            mediator.transfer(mediator_fee);
-            
-        }
-        
-        status = 3;
-        
-        end_auction == 0;
+		status = 3;
+
+		end_auction == 0;
 		
 	}
 	
 	function withdraw() public{
         
-        require(seller == msg.sender);
-        require(status == 3);
+		require(seller == msg.sender);
+		require(status == 3);
+
+		seller.transfer(this.balance);
        
-        seller.transfer(this.balance);
-       
-    }
+    	}
 		
 	// MEDIATION
 	
 	function invoke_mediation() public{
         
-        require(seller == msg.sender || bidder == msg.sender);
-        require(status == 1);
-        require( this.balance >= ( current_bid + mediator_fee ) );
-        
-        seller.transfer(current_bid);
-        
-        if(mediation_invoked == 1){
-            
-            mediator.transfer(mediator_fee);
-            
-        }
-        
-        status = 2;
-        
-        mediation_invoked = 1;
-        
-    }
+		require(seller == msg.sender || bidder == msg.sender);
+		require(status == 1);
+		require( this.balance >= ( current_bid + mediator_fee ) );
+
+		seller.transfer(current_bid);
+
+		if(mediation_invoked == 1){
+
+		    mediator.transfer(mediator_fee);
+
+		}
+
+		status = 2;
+
+		mediation_invoked = 1;
+
+	}
     
-    function mediation_end_auction(uint _seller_fee, uint _bidder_fee) public{
+    	function mediation_end_auction(uint _seller_fee, uint _bidder_fee) public{
      
-        require(status == 2);
-        require(msg.sender == mediator);
-		require( this.balance >= ( _seller_fee + _bidder_fee + mediator_fee ) );
+		require(status == 2);
+		require(msg.sender == mediator);
+			require( this.balance >= ( _seller_fee + _bidder_fee + mediator_fee ) );
+
+
+		seller.transfer(_seller_fee);
+		bidder.transfer(_bidder_fee);
+		mediator.transfer(mediator_fee);
+
+		status = 3;
         
-		
-        seller.transfer(_seller_fee);
-        bidder.transfer(_bidder_fee);
-        mediator.transfer(mediator_fee);
-        
-        status = 3;
-        
-    }
+    	}
 	
 	function mediation_remove_bidder() public{
      
-        require(status == 2);
-        require(msg.sender == mediator);
-        
-        bidder = 0x0;
+		require(status == 2);
+		require(msg.sender == mediator);
+
+		bidder = 0x0;
 		current_bid = 0;
         
-    }
+    	}
 	
 	function mediation_end() public{
      
-        require(status == 2);
-        require(msg.sender == mediator);
-        
-        status = 1;
-        
-    }
+		require(status == 2);
+		require(msg.sender == mediator);
+
+		status = 1;
+        }
 	
- 
-}
+ }
